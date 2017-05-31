5adb66308883e9c89656e9763e6654950d067456
hwzss <1833361588@qq.com>
2017-05-31 09:01:03 +0800

    fix bug of swizzle method

--- 集成App推送学习/集成App推送学习/WZ_AppNoticeHandler/WZ_AppNoticeManger.m
+++ 集成App推送学习/集成App推送学习/WZ_AppNoticeHandler/WZ_AppNoticeManger.m
@@ -17,177 +17,220 @@
 #define WZ_IOS8_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 #define WZ_IOS7_OR_LATER ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
 
-@interface WZ_AppNoticeManger ()<UNUserNotificationCenterDelegate>
+@interface WZ_AppNoticeManger () <UNUserNotificationCenterDelegate>
 
-@property(weak,nonatomic)UIApplication *application;
+@property (weak, nonatomic) UIApplication *application;
 
-@property(copy,nonatomic)NSString *devicePushToken;
+@property (copy, nonatomic) NSString *devicePushToken;
 
-@property(copy,nonatomic)WZ_fetchPushTokenBlock fetchPushTokenblock;
+@property (copy, nonatomic) WZ_fetchPushTokenBlock fetchPushTokenblock;
 @end
 
 @implementation WZ_AppNoticeManger
 
 static id _instance;
-+(instancetype)allocWithZone:(struct _NSZone *)zone{
-    _instance=[super allocWithZone:zone];
++ (instancetype)allocWithZone:(struct _NSZone *)zone
+{
+    _instance = [super allocWithZone:zone];
     return _instance;
 }
-+(instancetype)defaultManger{
-    if(!_instance){
-        _instance=[[[self class] alloc]init];
++ (instancetype)defaultManger
+{
+    if (!_instance)
+    {
+        _instance = [[[self class] alloc] init];
     }
     return _instance;
 }
--(UIApplication *)application{
-    if (!_application) {
+- (UIApplication *)application
+{
+    if (!_application)
+    {
         _application = [UIApplication sharedApplication];
     }
     return _application;
 }
--(void)setDevicePushToken:(NSString *)devicePushToken{
+- (void)setDevicePushToken:(NSString *)devicePushToken
+{
     _devicePushToken = devicePushToken;
-    if (_fetchPushTokenblock) {
+    if (_fetchPushTokenblock)
+    {
         _fetchPushTokenblock(devicePushToken);
         _fetchPushTokenblock = nil;
     }
 }
--(void)WZ_fetchPushToken:(WZ_fetchPushTokenBlock )fetchBlock{
-    if (self.devicePushToken) {
-        if (fetchBlock) {
+- (void)WZ_fetchPushToken:(WZ_fetchPushTokenBlock)fetchBlock
+{
+    if (self.devicePushToken)
+    {
+        if (fetchBlock)
+        {
             fetchBlock(self.devicePushToken);
         }
-    }else{
+    }
+    else
+    {
         self.fetchPushTokenblock = fetchBlock;
     }
 }
-#pragma -mark 注册远程通知
--(void)WZ_registerForRemoteNotifications{
-   
-    if (WZ_IOS10_OR_LATER) {
-        UNUserNotificationCenter *center  = [UNUserNotificationCenter currentNotificationCenter];
+#pragma - mark 注册远程通知
+- (void)WZ_registerForRemoteNotifications
+{
+
+    if (WZ_IOS10_OR_LATER)
+    {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
         center.delegate = self;
-        [center requestAuthorizationWithOptions:UNAuthorizationOptionAlert|UNAuthorizationOptionBadge|UNAuthorizationOptionSound completionHandler:^(BOOL granted, NSError * _Nullable error) {
-            if (granted) {
-                
-            }else{
-                
-            }
-        }];
+        [center requestAuthorizationWithOptions:UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound
+                              completionHandler:^(BOOL granted, NSError *_Nullable error) {
+                                  if (granted)
+                                  {
+                                  }
+                                  else
+                                  {
+                                  }
+                              }];
         [self.application registerForRemoteNotifications];
-    }else if (WZ_IOS8_OR_LATER){
-        UIUserNotificationSettings *setting = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert|UIUserNotificationTypeSound|UIUserNotificationTypeBadge categories:nil];
+    }
+    else if (WZ_IOS8_OR_LATER)
+    {
+        UIUserNotificationSettings *setting = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge categories:nil];
         [self.application registerUserNotificationSettings:setting];
         [self.application registerForRemoteNotifications];
-    }else{
-        [self.application registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeSound|UIRemoteNotificationTypeBadge];
     }
-    
+    else
+    {
+        [self.application registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeBadge];
+    }
 }
 
--(void)handlerNotificationWithNUserInfo:(NSDictionary *)userInfo IsTap:(WZ_AppNoticeTapType )tapType{
-   //default do nothing
+- (void)handlerNotificationWithNUserInfo:(NSDictionary *)userInfo IsTap:(WZ_AppNoticeTapType)tapType
+{
+    //default do nothing
 }
 
-#pragma -mark UNUserNotificationCenterDelegate
--(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler{
-    
+#pragma - mark UNUserNotificationCenterDelegate
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
+
     // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以设置
-    completionHandler(UNNotificationPresentationOptionBadge|
-                      UNNotificationPresentationOptionSound|
+    completionHandler(UNNotificationPresentationOptionBadge |
+                      UNNotificationPresentationOptionSound |
                       UNNotificationPresentationOptionAlert);
 }
--(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler{
-    
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler
+{
+
     [self handlerNotificationWithNUserInfo:response.notification.request.content.userInfo IsTap:WZ_NoticeTaped];
-    
+
     completionHandler();
 }
 
 @end
 
-@interface AppDelegate (WZ_handlerNotice)<UIApplicationDelegate>
+@interface AppDelegate (WZ_handlerNotice) <UIApplicationDelegate>
 
 @end
 
-BOOL classSwizzleInstanceMethod(Class aClass, SEL originalSel,SEL swizzleSel){
-    
+BOOL classSwizzleInstanceMethod(Class aClass, SEL originalSel, SEL swizzleSel)
+{
+
     Method originalMethod = class_getInstanceMethod(aClass, originalSel);
     Method swizzleMethod = class_getInstanceMethod(aClass, swizzleSel);
-    
-    BOOL didAddMethod = class_addMethod(aClass, originalSel, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
-    if (didAddMethod) {
-        class_replaceMethod(aClass, swizzleSel, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
-//        method_setImplementation(swizzleMethod, imp_implementationWithBlock(id block))
-    }else{
-        method_exchangeImplementations(originalMethod, swizzleMethod);
+
+    if (!originalMethod)
+    {
+        class_addMethod(aClass, originalSel, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
+        method_setImplementation(swizzleMethod, imp_implementationWithBlock(^(id self, SEL _cmd){
+                                                }));
     }
-    
+    else
+    {
+        BOOL didAddMethod = class_addMethod(aClass, originalSel, method_getImplementation(swizzleMethod), method_getTypeEncoding(swizzleMethod));
+        if (didAddMethod)
+        {
+            class_replaceMethod(aClass, swizzleSel, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+            //        method_setImplementation(swizzleMethod, imp_implementationWithBlock(id block))
+        }
+        else
+        {
+            method_exchangeImplementations(originalMethod, swizzleMethod);
+        }
+    }
+
     return YES;
 }
 
 @implementation AppDelegate (WZ_handlerNotice)
 
-+(void)load{
++ (void)load
+{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         Class aClass = [self class];
-        
+
         //hook launching to configure
         classSwizzleInstanceMethod(aClass, @selector(application:didFinishLaunchingWithOptions:), @selector(WZ_application:didFinishLaunchingWithOptions:));
         //hook register
         classSwizzleInstanceMethod(aClass, @selector(application:didRegisterForRemoteNotificationsWithDeviceToken:), @selector(WZ_application:didRegisterForRemoteNotificationsWithDeviceToken:));
         classSwizzleInstanceMethod(aClass, @selector(application:didFailToRegisterForRemoteNotificationsWithError:), @selector(WZ_application:didFailToRegisterForRemoteNotificationsWithError:));
-        
-        if (!WZ_IOS10_OR_LATER) {
+
+        if (!WZ_IOS10_OR_LATER)
+        {
             classSwizzleInstanceMethod(aClass, @selector(application:didReceiveRemoteNotification:), @selector(WZ_application:didReceiveRemoteNotification:));
-            classSwizzleInstanceMethod(aClass, @selector(WZ_application:didReceiveLocalNotification:), @selector(WZ_application:didReceiveLocalNotification:));
+            classSwizzleInstanceMethod(aClass, @selector(application:didReceiveLocalNotification:), @selector(WZ_application:didReceiveLocalNotification:));
         }
     });
 }
-#pragma -mark recive notice  and configure AppNoticehandler
--(BOOL)WZ_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
-    
+#pragma - mark recive notice and configure AppNoticehandler
+- (BOOL)WZ_application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+
     //configure register
     [[WZ_AppNoticeManger defaultManger] WZ_registerForRemoteNotifications];
-    
-    if (!WZ_IOS10_OR_LATER) {
+
+    if (!WZ_IOS10_OR_LATER)
+    {
         //iOS上，可以在didReceiveNotificationResponse接收到点击返回
-        NSDictionary *remoteUserInfo =[launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-        NSDictionary *LocalUserInfo =[launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
-        if (remoteUserInfo||LocalUserInfo) {
-            [[WZ_AppNoticeManger defaultManger] handlerNotificationWithNUserInfo:remoteUserInfo?remoteUserInfo:LocalUserInfo IsTap:WZ_NoticeTaped];
+        NSDictionary *remoteUserInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+        NSDictionary *LocalUserInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
+        if (remoteUserInfo || LocalUserInfo)
+        {
+            [[WZ_AppNoticeManger defaultManger] handlerNotificationWithNUserInfo:remoteUserInfo ? remoteUserInfo : LocalUserInfo IsTap:WZ_NoticeTaped];
         }
     }
 
     return [self WZ_application:application didFinishLaunchingWithOptions:launchOptions];
-    
 }
 
-#pragma -mark register Token
--(void)WZ_application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
-    
+#pragma - mark register Token
+- (void)WZ_application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+
     NSString *deviceTokenStr = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
     deviceTokenStr = [deviceTokenStr stringByReplacingOccurrencesOfString:@" " withString:@""];
     [WZ_AppNoticeManger defaultManger].devicePushToken = deviceTokenStr;
-    
+
     [self WZ_application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
 }
--(void)WZ_application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
-    
+- (void)WZ_application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+
     [self WZ_application:application didFailToRegisterForRemoteNotificationsWithError:error];
 }
-#pragma -mark recevice and handle notification
--(void)WZ_application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
-    
+#pragma - mark recevice and handle notification
+- (void)WZ_application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+
     [[WZ_AppNoticeManger defaultManger] handlerNotificationWithNUserInfo:userInfo IsTap:WZ_NoticeTapUnkown];
-    
+
     [self WZ_application:application didReceiveRemoteNotification:userInfo];
 }
--(void)WZ_application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
-    
+- (void)WZ_application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+
     [[WZ_AppNoticeManger defaultManger] handlerNotificationWithNUserInfo:notification.userInfo IsTap:WZ_NoticeTapUnkown];
-    
+
     [self WZ_application:application didReceiveLocalNotification:notification];
 }
 
